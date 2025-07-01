d2926b1b4c04360248c4fa81b2647e4fe7b40ce5
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-01 10:30:50 +0900

    練習問題3-1

---
+++ src/RectAndOval.java
@@ -0,0 +1,9 @@
+
+public class RectAndOval extends MyFrame {
+	public void run() {
+			fillRect(60,100,70,20);
+			fillRect(75,80,40,20);
+			fillOval(70, 120, 20, 20);
+			fillOval(100, 120, 20, 20);
+	}
+}
--- src/Start.java
+++ src/Start.java
@@ -3,15 +3,9 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int x = 2;
-		int y = 3;
-		System.out.println(x * y);
-
-		MyFrame frame1 = new RectFrame();
-		frame1.setLocation(200,100);
-		MyFrame frame2 = new RectFrame();
-		frame2.setLocation(350,100);
-		
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectAndOval();
+			
 	}
 
 }
