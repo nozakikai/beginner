f0a30a85440ddaffd8c2208a808caa1341a519d0
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-01 11:04:36 +0900

    練習問題4-3

--- src/RectLoop.java
+++ src/RectLoop.java
@@ -1,11 +1,11 @@
 
 public class RectLoop extends MyFrame {
 	public void run() {
-		setColor(0, 128, 0);
+		
 		int x = 30;
 		for (int i=0; i <10 ; i++) {
-
-			fillRect(x, 80, 10, x);
+			setColor(x, x, x);
+			fillRect(x, 80, 10, 100);
 			x+=20;
 			
 		}
