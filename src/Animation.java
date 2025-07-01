e416b08db1a64f797aee9d8210ec50ba4d901647
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-01 11:51:51 +0900

    練習問題5-2

--- src/Animation.java
+++ src/Animation.java
@@ -2,25 +2,36 @@
 public class Animation extends MyFrame {
 	public void run() {
 		int x = 60;
-		int y = 180;
+		int y = 40;
 
-		while (y > 40) { //永久に繰り返す
-			//(1)画面を消す
-			clear();
-			//(2)四角を表示する
-			setColor(0, 128, 0);
-			fillRect(x, y, 10, 100);
-			y-=5;
-			sleep(0.01);
-		}
-		
-		while (x <= 300) { //永久に繰り返す
+		while (y <= 280) { //永久に繰り返す
 			//(1)画面を消す
 			clear();
 			//(2)四角を表示する
 			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100);
 			x+=5;
+			y+=5;
+			sleep(0.01);
+		}
+		
+		while (x >= 30) { //永久に繰り返す
+			//(1)画面を消す
+			clear();
+			//(2)四角を表示する
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x-=5;
+			sleep(0.01);
+		}
+		while (y >= 30) { //永久に繰り返す
+			//(1)画面を消す
+			clear();
+			//(2)四角を表示する
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x+=5;
+			y-=5;
 			sleep(0.01);
 		}
 
