60e47f979d1b4afa63e71793ce5eb5d0cb6ec75a
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-01 09:52:16 +0900

    練習問題2-1

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,10 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("山田太郎");
+		int x = 2;
+		int y = 3;
+
+		System.out.println(x * y);
 	}
 
 }
