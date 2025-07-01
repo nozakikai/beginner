d9bc234dcba4f639c435bdbea4530a587e0fa590
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-01 09:56:59 +0900

    練習問題2-2

--- src/Start.java
+++ src/Start.java
@@ -5,8 +5,13 @@ public class Start {
 		// TODO 自動生成されたメソッド・スタブ
 		int x = 2;
 		int y = 3;
-
 		System.out.println(x * y);
+
+		MyFrame frame1 = new RectFrame();
+		frame1.setLocation(200,100);
+		MyFrame frame2 = new RectFrame();
+		frame2.setLocation(350,100);
+		
 	}
 
 }
