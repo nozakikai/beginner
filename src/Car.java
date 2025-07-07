c35d455fda25dc28115e9efa9d24b0720ba34aeb
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-07-07 12:09:53 +0900

    練習問題9-1

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements KeyListener {
 
 	public Car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
@@ -11,6 +13,46 @@ public class Car extends Vehicle {
 		frame.fillRect(x, y + 20, 90, 20);
 		frame.fillOval(x + 15, y + 40, 15, 15);
 		frame.fillOval(x + 50, y + 40, 15, 15);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if ((e.getKeyCode() == KeyEvent.VK_LEFT) || (e.getKeyCode() == KeyEvent.VK_A)) {
+			vx = -10;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_RIGHT) || (e.getKeyCode() == KeyEvent.VK_D)) {
+			vx = 10;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_UP) || (e.getKeyCode() == KeyEvent.VK_W)) {
+			vy = -10;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_DOWN) || (e.getKeyCode() == KeyEvent.VK_S)) {
+			vy = 10;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if ((e.getKeyCode() == KeyEvent.VK_LEFT) || (e.getKeyCode() == KeyEvent.VK_A)) {
+			vx = 0;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_RIGHT) || (e.getKeyCode() == KeyEvent.VK_D)) {
+			vx = 0;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_UP) || (e.getKeyCode() == KeyEvent.VK_W)) {
+			vy = 0;
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_DOWN) || (e.getKeyCode() == KeyEvent.VK_S)) {
+			vy = 0;
+		}
 	}
 
 }
